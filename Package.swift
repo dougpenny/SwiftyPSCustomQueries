430c6c523fe6b2dbab4c87b18c0ca0bd6d429dfa
Doug Penny <penny@nrcaknights.com>
2020-05-20 15:28:46 -0400

    Correct dependency location

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         .library(name: "SwiftyPSCustomQueries", targets: ["SwiftyPSCustomQueries"]),
     ],
     dependencies: [
-        .package(name: "SwiftyPSCore", path: "../SwiftyPowerSchool-Core"),
+        .package(url: "https://github.com/nrca/SwiftyPSCore.git", from: "1.0.0-beta4"),
     ],
     targets: [
         .target(name: "SwiftyPSCustomQueries", dependencies: ["SwiftyPSCore"]),
